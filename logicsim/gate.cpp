340b3fce8ceee63179282a7775a75e99fa72a46c
Fred Pan <fredpan@usc.edu>
2023-02-24 02:28:35 +0000

    all done

--- logicsim/gate.cpp
+++ logicsim/gate.cpp
@@ -96,18 +96,20 @@ NotGate::NotGate(Wire* a, Wire* o) : Gate(1,o)
 
 Event* NotGate::update(uint64_t current_time)
 {
-	char state = '0';
+	
+	char state = '1';
   Event* e = nullptr;
 	for(auto w : m_inputs)
 	{
 		char in = w->getState();
-		if(in == '1')
+		if(in == '0')
 		{
-			state = '0';
+			state = '1';
 			break;
 		}
-		else if(in == '0') {
-			state = '1';
+		else if(in == '1') 
+		{
+			state = '0';
 			break;
 		}
 		else if(in == 'X')
@@ -115,12 +117,12 @@ Event* NotGate::update(uint64_t current_time)
 			state = 'X';
 		}
 	}
-  if(state != m_current_state)
+    if(state != m_current_state)
 	{
     m_current_state = state;
-		uint64_t next = current_time + m_delay;
-		e = new Event {next,m_output,state}; 
-		
+    uint64_t next = current_time + m_delay;
+		e = new Event {next,m_output,state};
+         
 	}
-  return e;
+    return e;
 }
